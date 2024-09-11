06db8b6f66560accc50955069f06073b00e776ca
hatemondaysandcops <hatemondaysandcops@gmail.com>
2024-09-11 18:46:39 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,24 @@
-import { postImage } from './clients/at.js'; // Adicione a extensão .js para módulos ES
-import { getNextImage } from './images.js'; // Adicione a extensão .js para módulos ES
+import { postImage } from './clients/at'; // Importing from TypeScript file in clients folder
+import { getNextImage } from './images';   // Importing from TypeScript file in the same directory
 import * as dotenv from 'dotenv';
 
-// Carregar variáveis de ambiente
 dotenv.config();
 
-// Função principal para obter a próxima imagem e postá-la
 async function main() {
-  // Obtendo o nome da última imagem a partir das variáveis de ambiente
   const { LAST_IMAGE_NAME: lastImageName } = process.env;
 
-  // Obtendo a próxima imagem com base no nome da última imagem
   const nextImage = await getNextImage({ lastImageName });
 
-  // Log do nome da próxima imagem
   console.log(nextImage.imageName);
 
-  // Postando a próxima imagem
   await postImage({
     path: nextImage.absolutePath,
-    text: '', // Nenhum texto para o post da imagem
-    altText: '', // Nenhum texto alternativo para o post da imagem
+    text: '',
+    altText: '',
   });
 }
 
-// Executar a função principal
 main().catch(error => {
   console.error('Erro ao executar o script:', error);
-  process.exit(1); // Sair com um código de erro
+  process.exit(1);
 });
