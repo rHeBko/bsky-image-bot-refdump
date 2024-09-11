faf62230b94c91079142d91d1355e375bf71b924
hatemondaysandcops <hatemondaysandcops@gmail.com>
2024-09-10 23:48:13 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,7 @@ import { getNextImage } from './images';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-// EDIT THIS!
-function postTextFromImageName(imageName: string): string {
-  // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
-  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
-
-  // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-
-  // Format the date
-  return formatter.format(date);
-}
-
-// EDIT THIS!
-function altTextFromImageName(imageName: string): string {
-  return 'Image from ' + postTextFromImageName(imageName);
-}
-
-// Shouldn't have to edit this.
+// Function to get the next image without additional text
 async function main() {
   const { LAST_IMAGE_NAME: lastImageName } = process.env;
   const nextImage = await getNextImage({ lastImageName });
@@ -34,8 +12,8 @@ async function main() {
 
   await postImage({
     path: nextImage.absolutePath,
-    text: postTextFromImageName(nextImage.imageName),
-    altText: altTextFromImageName(nextImage.imageName),
+    text: '', // No text for the image post
+    altText: '', // No alt text for the image post
   });
 }
 
