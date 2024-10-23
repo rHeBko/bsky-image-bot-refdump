f97446c5401d2a1b24f2d933b62bc6cd7d2c7cea
byte:FrHebKa <wrathofcat@gmail.com>
2024-10-23 15:25:55 -0700

    metadata formatting tweaks

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ function postTextFromImageName(imageName: string): string {
 
 // EDIT THIS!
 function altTextFromImageName(imageName: string): string {
-  return 'Image from ' + postTextFromImageName(imageName);
+  return 'Image title ' + postTextFromImageName(imageName);
 }
 
 // Shouldn't have to edit this.
